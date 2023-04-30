d61e4a7d860ee8ea980a33b5c0bc17284d8c6965
Regie <reginapanina@mail.ru>
2023-04-30 15:22:33 +0300

    Build almost success

--- src/test/java/ru/netology/SeleniumXTest.java
+++ src/test/java/ru/netology/SeleniumXTest.java
@@ -28,7 +28,7 @@ class SeleniumXTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
@@ -40,7 +40,6 @@ class SeleniumXTest {
 
     @Test
     void getPositiveResult() {
-        driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Виктор");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79134567583");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
